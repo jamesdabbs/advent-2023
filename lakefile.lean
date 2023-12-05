5376c062b5ff588235d2073efac14dfb8ed56fa5
James Dabbs <james.dabbs@gmail.com>
2023-12-04 16:28:28 -0800

    initial scaffolding

---
+++ lakefile.lean
@@ -0,0 +1,16 @@
+import Lake
+open Lake DSL
+
+package «advent-2023» where
+  -- add package configuration options here
+
+lean_lib «Advent2023» where
+  -- add library configuration options here
+
+@[default_target]
+lean_exe «advent-2023» where
+  root := `Main
+  -- Enables the use of the Lean interpreter by the executable (e.g.,
+  -- `runFrontend`) at the expense of increased binary size on Linux.
+  -- Remove this line if you do not need such functionality.
+  supportInterpreter := true
